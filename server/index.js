6d9077ab9a452ab6e89600ffae173aa0dc0279b9
Stanley Ejingiri <stanleyejingiri@gmail.com>
2026-06-07 17:37:51 -0400

    Add frontend URL to CORS allowed origins

--- server/index.js
+++ server/index.js
@@ -45,6 +45,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
 });
 
 // CORS MUST come before routes
+/*
 app.use(cors({
   origin: [
     'http://localhost:8080', 
@@ -54,6 +55,28 @@ app.use(cors({
     'http://localhost:3000',
     'http://127.0.0.1:3000'
   ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: [
+    'Content-Type', 
+    'Authorization', 
+    'user-data',
+    'x-client-info'
+  ]
+}));*/
+
+// CORS MUST come before routes
+app.use(cors({
+  origin: [
+    'http://localhost:8080', 
+    'http://127.0.0.1:8080',
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'https://one2fingers-frontend.onrender.com',
+    process.env.FRONTEND_URL
+  ].filter(Boolean),
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: [
