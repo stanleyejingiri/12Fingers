abce0d16efb4967c798e05b8b9d61a955546b6c0
Stanley Ejingiri <stanleyejingiri@gmail.com>
2026-06-11 14:19:34 -0400

    Fix CSS paths with base '/'

--- vite.config.ts
+++ vite.config.ts
@@ -44,45 +44,13 @@ export default defineConfig(({ mode }) => ({
 */
 
 //vite.config.ts
-/*
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-      },
-    },
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-  },
-  define: {
-    'process.env': process.env
-  }
-}));
-*/
-
-//vite.config.ts
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-
-export default defineConfig(({ mode }) => ({
-  base: './',
+	base: '/',
+  /*base: './',*/
   server: {
     host: "::",
     port: 8080,
